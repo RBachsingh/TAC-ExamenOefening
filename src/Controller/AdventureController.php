018a55e42110e2d9f6ec488c5353d7a5c8072e38
RBachsingh <302777281@student.rocmondriaan.nl>
2024-04-19 15:12:36 +0200

    oefeningen voor examen, moet nog homepage en redirect maken voor users.

---
+++ src/Controller/AdventureController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AdventureController extends AbstractController
+{
+    #[Route('/', name: 'adventure')]
+    public function index(): Response
+    {
+        return $this->render('adventure/index.html.twig', [
+            'controller_name' => 'AdventureController',
+        ]);
+    }
+
+}
